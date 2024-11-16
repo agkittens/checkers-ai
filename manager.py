5b01a774726a7e62dedf896057a4ea63726aa981
StainedMentor <olilosvik@gmail.com>
2024-11-16 11:07:39 +0100

    Send moves to robot

--- manager.py
+++ manager.py
@@ -5,11 +5,16 @@ class Manager:
         self.active_connections = {"mitsubishi":None, "kawasaki":None, "others":None}
 
     def add_robot(self, name: str):
-        self.active_connections[name] = Robot()
+        self.active_connections[name] = Robot(target=name)
 
-    def transmit(self, name: str):
-        self.active_connections[name].send_data()
-
+    def transmit(self, name: str, p1,p2):
+        try:
+            self.active_connections[name].send_data(p1,p2)
+        except:
+            print("No connection to robot")
     def receive(self, name: str):
-        self.active_connections[name].receive_status()
+        try:
+            self.active_connections[name].receive_status()
+        except:
+            print("No connection to robot")
 
